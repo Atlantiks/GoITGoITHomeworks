565c827f1342d11ab03f5f365bbcd93b7e9ece1a
Sergii Shynkarenko <atlantiks@outlook.com>
2022-06-16 12:43:01 +0300

    Tasks 1 & 2 complete for Homework 10

---
+++ src/homework10/Main.java
@@ -0,0 +1,27 @@
+package homework10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Ivan", "Peter", "Andrew", "John", "Kevin");
+        System.out.println(oddNamesList(names));
+        System.out.println(switchToUpperCaseAndSort(names));
+    }
+
+    public static String oddNamesList(List<String> names) {
+        final int[] counter = {1};
+        return names.stream().reduce("", (a, b) -> {
+            if (counter[0]++ % 2 != 0) return a + (counter[0] - 1) + ". " + b + " ";
+            else return a;
+        }).trim();
+    }
+
+    public static String switchToUpperCaseAndSort(List<String> names) {
+        return names.stream()
+                .map(String::toUpperCase).sorted()
+                .reduce("", (x, y) -> x + " " + y)
+                .trim();
+    }
+}
